f39798bdcc9b47a83cd8dbf5099564efb8ab3d53
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-07 14:55:22 +0530

    useDispatch Hook Created

--- src/components/PlusMinus.js
+++ src/components/PlusMinus.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { increment, decrement } from '../action/index'
 export default function PlusMinus() {
+    const dispatch = useDispatch()
     return (
         <div>
             <h1>Function Comonents</h1>
             <div className='box'>
-                <button onClick={() => increment()}>+</button>
+                <button onClick={() => dispatch(increment())}>+</button>
                 <p>0</p>
-                <button onClick={() => decrement()}>-</button>
+                <button onClick={() => dispatch(decrement())}>-</button>
             </div>
         </div>
     )
--- src/reducers/UpDown.js
+++ src/reducers/UpDown.js
@@ -2,6 +2,7 @@ import { DECREMENT, INCREMENT } from "../constants";
 
 const initialState = 0;
 const changeCount = (state = initialState, action) => {
+    console.log(action);
     switch (action.type) {
         case INCREMENT:
             return state + 1;
