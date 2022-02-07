eec387c57b534607d458ba6381bb89fea3aac219
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-07 10:54:28 +0530

    Project Setup function and class

---
+++ src/components/PlusMinus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function PlusMinus() {
+    return (
+        <div>
+            <h1>Function Comonents</h1>
+            <div className='box'>
+                <button>-</button>
+                <p>0</p>
+                <button>+</button>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/PlusMinusDemo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class PlusMinusDemo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Class Comonents</h1>
+                <div className='box'>
+                    <button>-</button>
+                    <p>0</p>
+                    <button>+</button>
+                </div>
+            </div>
+        )
+    }
+}
