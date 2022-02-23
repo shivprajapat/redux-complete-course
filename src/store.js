d9631f5405fbbcdba8de88178c7afd103a4d8fe7
shivprajapat <shivkumarprajapat232@gmail.com>
2022-02-23 09:27:55 +0530

    local storage use redux-persist package

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
 import { createStore } from "redux";
 import { rootReducers } from "./reducers";
-const store = createStore(rootReducers);
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
+const persistConfig = {
+    key: 'persist-root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducers);
+const store = createStore(persistedReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export const persistor = persistStore(store)
 export default store
